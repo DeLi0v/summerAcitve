3e2f4d24899b87f7a01ea23c30b9bdbb9e245e4a
DeLi0v <denov78@mail.ru>
2025-04-14 05:31:12 +0300

    first pages

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "192.168.0.2";
+$dbname = "rental_db";
+$username = "webuser";
+$password = "1";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // Включим режим ошибок
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Ошибка подключения к базе данных: " . $e->getMessage());
+}
