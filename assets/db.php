765ddb9e06e23c4a187fb535c33d2790fc59c99b
DeLi0v <denov78@mail.ru>
2025-04-16 11:34:14 +0300

    debug connect

--- assets/db.php
+++ assets/db.php
@@ -4,6 +4,8 @@ $dbname = "rental_db";
 $username = "webuser";
 $password = "1";
 
+echo "mysql:host=$host;dbname=$dbname;charset=utf8";
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
