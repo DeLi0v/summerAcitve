6c85f68089d1f49da212987b9b4495b8416a646d
DeLi0v <denov78@mail.ru>
2025-04-16 08:35:49 +0300

    test work styles

--- templates/header.php
+++ templates/header.php
@@ -6,15 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($page_title) ? $page_title : 'Аренда оборудования'; ?></title>
     <link rel="stylesheet" href="/assets/styles.css">
+    <style>
+        .test2 {
+            color: #14fa00;
+        }
+    </style>
 </head>
 <body>
     <header>
         <div class="logo">
-            <a href="index.php" class="test">Аренда оборудования</a>
+            <a href="/index.php" class="test">Аренда оборудования</a>
         </div>
         <nav>
             <ul>
-                <li><a href="index.php">Каталог</a></li>
+                <li><a href="/index.php">Каталог</a></li>
                 <?php if (isset($_SESSION['user_id'])): ?>
                     <li><a href="#">Личный кабинет</a></li>
                     <li><a href="#">Выход</a></li>
@@ -23,5 +28,8 @@
                     <li><a href="#">Регистрация</a></li>
                 <?php endif; ?>
             </ul>
+            <div class="test2">
+                <p>test2</p>
+            </div>
         </nav>
     </header>
