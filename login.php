7dcb0e678f1bafac4b5d6cf3e5214e46d4092970
DeLi0v <denov78@mail.ru>
2025-04-16 09:59:40 +0300

    fix connect

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = trim($_POST['email']);
     $password = $_POST['password'];
 
-    $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
     $stmt->execute([$email]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
