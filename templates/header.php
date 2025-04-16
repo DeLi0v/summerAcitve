4cab8a0190ce55c41617fc3658f9d9db5f628cb2
DeLi0v <denov78@mail.ru>
2025-04-16 08:28:32 +0300

    fix styles

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($page_title) ? $page_title : 'Аренда оборудования'; ?></title>
-    <link rel="stylesheet" href="assets/styles.css">
+    <link rel="stylesheet" href="/assets/styles.css">
 </head>
 <body>
     <header>
@@ -16,11 +16,11 @@
             <ul>
                 <li><a href="index.php">Каталог</a></li>
                 <?php if (isset($_SESSION['user_id'])): ?>
-                    <li><a href="/panel.php">Личный кабинет</a></li>
-                    <li><a href="logout.php">Выход</a></li>
+                    <li><a href="#">Личный кабинет</a></li>
+                    <li><a href="#">Выход</a></li>
                 <?php else: ?>
-                    <li><a href="login.php">Вход</a></li>
-                    <li><a href="register.php">Регистрация</a></li>
+                    <li><a href="#">Вход</a></li>
+                    <li><a href="#">Регистрация</a></li>
                 <?php endif; ?>
             </ul>
         </nav>
