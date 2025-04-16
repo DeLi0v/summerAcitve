4fff55d14389426a09854276520846ef3d68eebd
DeLi0v <denov78@mail.ru>
2025-04-16 11:24:26 +0300

    add info

--- login.php
+++ login.php
@@ -13,9 +13,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = trim($_POST['email']);
     $password = $_POST['password'];
 
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt = mysqli_query($conn, "SELECT * FROM users WHERE email = ?");
     $stmt->execute([$email]);
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    $user = mysqli_fetch_all($stmt, MYSQLI_ASSOC);
 
     if ($user && password_verify($password, $user['password_hash'])) {
         $_SESSION['user_id'] = $user['id'];
