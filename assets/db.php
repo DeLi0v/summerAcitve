ea567d299209ea32d60539aa716a282af3e48de4
DeLi0v <denov78@mail.ru>
2025-04-16 09:55:41 +0300

    change db

--- assets/db.php
+++ assets/db.php
@@ -1,13 +1,21 @@
 <?php
-$host = "192.168.0.2";
-$dbname = "rental_db";
-$username = "webuser";
-$password = "1";
+var $host = "192.168.0.2";
+var $dbname = "rental_db";
+var $username = "webuser";
+var $password = "1";
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    // Включим режим ошибок
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Ошибка подключения к базе данных: " . $e->getMessage());
+var $conn;
+
+$conn = mysqli_connect($this->hostname, $this->username, $this->password, $this->database);
+// try {
+//     $conn = mysqli_connect($this->hostname, $this->username, $this->password, $this->database);
+//     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+//     // Включим режим ошибок
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//     die("Ошибка подключения к базе данных: " . $e->getMessage());
+// }
+
+if (!$this->conn) {
+    die("Подключение не удалось: " . mysqli_connect_error());
 }
