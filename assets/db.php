92cda7c2d1bda1e3b9ec17cb91bf06332a42c09e
DeLi0v <denov78@mail.ru>
2025-04-20 22:28:50 +0300

    del sql debug

--- assets/db.php
+++ assets/db.php
@@ -4,10 +4,6 @@ $dbname = "rental_db";
 $username = "webuser";
 $password = "1";
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
