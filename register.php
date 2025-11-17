f87728405a561f24e4b3f2120227602f0405cc2e
DeLi0v <denov78@mail.ru>
2025-11-17 23:25:48 +0300

    change type for phone input

--- register.php
+++ register.php
@@ -63,7 +63,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <input type="email" id="email" name="email" required>
 
         <label for="phone">Телефон:</label>
-        <input type="text" id="phone" name="phone" onkeypress="return isNumberKey(event)" required>
+        <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required>
 
         <label for="password">Пароль:</label>
         <input type="password" id="password" name="password" required>
