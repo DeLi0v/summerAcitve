f6cccdb8ad9f3e6b14ef0e445936d6386f351736
DeLi0v <109518046+DeLi0v@users.noreply.github.com>
2025-11-19 16:39:15 +0300

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,16 @@
 <!-- templates/header.php -->
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo isset($page_title) ? $page_title : 'Аренда оборудования'; ?></title>
+    <title>
+        <?php echo isset($page_title) ? $page_title : 'Аренда оборудования'; ?>
+    </title>
     <link rel="stylesheet" href="/styles/main.css">
 </head>
+
 <body>
     <header>
         <div class="logo">
@@ -14,17 +18,26 @@
         </div>
         <nav>
             <ul>
-                <li><a href="/index.php"><img src="/assets/catalogue.png" alt="Каталог" class="menu-icon">Каталог</a></li>
-                <?php if (isset($_SESSION['user_id'])): ?>
-                    <?php if ($_SESSION['role'] === 'admin'):?>
-                        <li><a href="/admin/panel.php"><img src="/assets/admin.png" alt="Административная панель" class="menu-icon">Административная панель</a></li>
-                    <?php else: ?>
-                        <li><a href="/account/account.php"><img src="/assets/account.png" alt="Личный кабинет" class="menu-icon">Личный кабинет</a></li>
-                    <?php endif; ?>
-                    <li style="margin-left: 25px;"><a href="/logout.php"><img src="/assets/logout.png" alt="Выход" class="menu-icon">Выход</a></li>
+                <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
+
+                    <!-- Админ-навигация -->
+                    <li><a href="/admin/users.php"><img src="/assets/users.png" alt="" class="menu-icon">Пользователи</a></li>
+                    <li><a href="/admin/equipment.php"><img src="/assets/equipment.png" alt="" class="menu-icon">Оборудование</a></li>
+                    <li><a href="/admin/bookings.php"><img src="/assets/bookings.png" alt="" class="menu-icon">Бронирования</a></li>
+                    <li><a href="/admin/categories.php"><img src="/assets/categories.png" alt="" class="menu-icon">Категории оборудования</a></li>
+                    <li style="margin-left: 25px;"><a href="/logout.php"><img src="/assets/logout.png" alt="" class="menu-icon">Выход</a></li>
+
                 <?php else: ?>
-                    <li><a href="/login.php"><img src="/assets/enter.png" alt="Вход" class="menu-icon">Вход</a></li>
-                    <!-- <li><a href="/register.php"><img src="/assets/catalogue.png" alt="Каталог" class="menu-icon">Регистрация</a></li> -->
+                
+                    <!-- Обычная навигация для пользователей -->
+                    <li><a href="/index.php"><img src="/assets/catalogue.png" alt="Каталог" class="menu-icon">Каталог</a></li>
+                    <?php if (isset($_SESSION['user_id'])): ?>
+                        <li><a href="/account/account.php"><img src="/assets/account.png" alt="Личный кабинет" class="menu-icon">Личный кабинет</a></li>
+                        <li style="margin-left: 25px;"><a href="/logout.php"><img src="/assets/logout.png" alt="Выход" class="menu-icon">Выход</a></li>
+                    <?php else: ?>
+                        <li><a href="/login.php"><img src="/assets/enter.png" alt="Вход" class="menu-icon">Вход</a></li>
+                    <?php endif; ?>
+
                 <?php endif; ?>
             </ul>
         </nav>
