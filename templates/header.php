0e65fca09e0e4a0ed11ea7ed609bd82e46af46fb
DeLi0v <109518046+DeLi0v@users.noreply.github.com>
2025-11-19 16:57:02 +0300

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -26,7 +26,6 @@
                     <li><a href="/admin/equipment.php">Оборудование</a></li>
                     <li><a href="/admin/bookings.php">Бронирования</a></li>
                     <li><a href="/admin/categories.php">Категории оборудования</a></li>
-                    <li style="margin-left: 25px;"><a href="/logout.php">Выход</a></li>
                 <?php endif; ?>
             </ul>
     </div>
@@ -39,7 +38,7 @@
                 <li><a href="/index.php"><img src="/assets/catalogue.png" class="menu-icon">Каталог</a></li>
                 <?php if (isset($_SESSION['user_id'])): ?>
                     <li><a href="/account/account.php"><img src="/assets/account.png" class="menu-icon">Личный кабинет</a></li>
-                    <li><a href="/logout.php"><img src="/assets/logout.png" class="menu-icon">Выход</a></li>
+                    <li style="margin-left: 25px;">><a href="/logout.php"><img src="/assets/logout.png" class="menu-icon">Выход</a></li>
                 <?php else: ?>
                     <li><a href="/login.php"><img src="/assets/enter.png" class="menu-icon">Вход</a></li>
                 <?php endif; ?>
